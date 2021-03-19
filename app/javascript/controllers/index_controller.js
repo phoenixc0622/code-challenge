6684ae0733102a1eed0da22e428c5e07fb319193
XiaoFeng An <phoenixc0622@gmail.com>
2021-03-18 20:12:49 -0700

    Implemented age calculation and data mapping

--- app/javascript/controllers/index_controller.js
+++ app/javascript/controllers/index_controller.js
@@ -1,9 +1,29 @@
 import { Controller } from 'stimulus'; 
 export default class extends Controller {
+  static targets = [
+    "label",
+    "input"
+  ]
   connect() {
-    console.log("Stimulus")
+    console.log("Stimulus");
   }
-  helloWorld() {
-    console.log("hello World")
+  onInputChange() {
+    console.log(this.inputTarget.value);
+    const year = parseInt(this.inputTarget.value);
+    const now = new Date();
+    const age = now.getFullYear() - year;
+    console.log(age);
+    if (isNaN(age) || age < 0) {
+      this.labelTarget.innerHTML = 'Age: Invalid';
+      this.labelTarget.classList.add("text-red-500");
+      return;
+    }
+    if (age < 18) {
+      console.log(this.labelTarget.classList);
+      this.labelTarget.classList.add("text-red-500");
+    } else {
+      this.labelTarget.classList.remove("text-red-500");
+    }
+    this.labelTarget.innerHTML = `Age: ${age}`;
   }
 }
